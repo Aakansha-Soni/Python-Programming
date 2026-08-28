b265a7a125b8a6641b8cb9c3514246eec8f4bffc
Aakansha Soni <ankusoni322@gmail.com>
2026-08-28 21:38:09 +0530

    Create 01_basics.py

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,21 @@
+#  In python , variables are used to store data that can be
+#  used and manipulated throughout a program. a variable is 
+#  created the moment you assign a value to it using the
+#  assignment operator (=)
+
+age = 34 # integer
+name = "Harry" # string
+cgpa = 4.55 # float
+
+# Rule of defining a variable in python
+# variable name must start with a letter (a-z, A-Z) or an underscore(_).
+#They can contain letters, numbers, and underscores.
+#Variable names are case-sensitive (age and Age are different).
+#Avoid using Python keywords (e.g., if, for, while) as variable names.
+
+# 34age = 4 # Invalid because variable cannot start with a number
+# age = 32 # Valid because variable can start with a number
+# a$$ge = 45 # Invalid because variables cannot contain special characters other than _
+__age = 34 #valid because it is following all the rules.
+__nice__45 = 34 #valid
+a_b_c_7 = "Sam" #valid
