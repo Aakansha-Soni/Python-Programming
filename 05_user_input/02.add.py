51698e336e1784346e2257d92b2762c469cc5d9c
Aakansha Soni <ankusoni322@gmail.com>
2026-09-09 15:50:31 +0530

    Add files via upload

---
+++ 05_user_input/02.add.py
@@ -0,0 +1,18 @@
+# a = input("Enter first number")
+# a = int(a)
+# print(a + 3) # covert a to int version of a
+
+
+# a = input("Enter first number")
+# a = int(a)
+# b = input("Enter second number")
+# b = int(b) # There is a better way to convert this in int form
+
+# print(a + b)
+
+a = int(input("Enter first number"))
+a = int(a)
+b = int(input("Enter second number"))
+b = int(b)
+
+print(a + b)
