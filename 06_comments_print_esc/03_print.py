cfbf09a448908bc6fc49500fb8eea1368d4718d2
Aakansha Soni <ankusoni322@gmail.com>
2026-09-12 17:52:33 +0530

    Add files via upload

---
+++ 06_comments_print_esc/03_print.py
@@ -0,0 +1,25 @@
+'''print('Hello " World')
+print('Hello \' World')
+print('Hello World', "Harry", 5)'''
+
+'''# IF YOU WILL GIVE MULTIPLE PRINT STATEMENT , IT IS GOING TO AUTOMATICALLY ADD A NEW LINE AFTER A SINGLE PRINT STATEMENT
+print('Hello World')
+print('Harry')
+# Can we override this behaviour? Yes! We can by using sep"," or 
+# you can give end = "!\n" '''
+
+'''#sep
+print('Hello World', "Harry", 5, sep=",")
+print('Hello World', "Harry", 5, sep="/")'''
+
+'''#End
+print('Hello World', end="\n")
+print('Harry')
+
+print('Hello World', end=",")
+print('Harry')
+
+print('Hello World', end="..")
+print('Harry', end="//")'''
+
+print("Hello", "World", sep=", ", end="!\n")
