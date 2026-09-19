3a94145bab0d60d0729e5ae053c582d5d8b29b04
Aakansha Soni <ankusoni322@gmail.com>
2026-09-19 21:22:13 +0530

    Add files via upload

---
+++ 08_conditionals/01_if_statement.py
@@ -0,0 +1,15 @@
+'''age = 32
+
+if(age>18):
+    print("You can drive")
+    print("Thank You ")
+
+print("End of program")'''
+
+age = 12
+
+if(age>18):
+    print("You can drive")
+    print("Thank You ")
+
+print("End of program")
