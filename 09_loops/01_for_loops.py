8da6019a1d4c007b39f3dac767eab47c26129ffe
Aakansha Soni <ankusoni322@gmail.com>
2026-09-24 13:47:02 +0530

    Add files via upload

---
+++ 09_loops/01_for_loops.py
@@ -0,0 +1,15 @@
+'''# Print numbers from 1 to 5
+for i in range(1, 6): # Range Function goes from 1 to n - 1 
+    print(i)  # In this case it goes from 1 to 5 (6-1).'''
+
+#Print table of 5(I method)
+for i in range(1, 11):
+    print(5*(i))
+
+#Print table of 5(II method)
+for i in range(1, 11):
+    print("5 X ",i, " = ", 5*(i))
+
+#Print table of 5(III method)
+for i in range(1, 11):
+    print("5 X ",i, " = ", 5*i)
